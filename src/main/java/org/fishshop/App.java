5147cdc7ee90ee926895ba87a1b1328ab89c46ba
belial <skywokerua@gmail.com>
2021-01-11 13:59:17 +0200

    create some setting to project and login window

---
+++ src/main/java/org/fishshop/App.java
@@ -0,0 +1,29 @@
+package org.fishshop;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root, 780, 439 ));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/org/fishshop/loginController.java
@@ -0,0 +1,26 @@
+package org.fishshop;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
+
+public class loginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label loginLabel;
+}
